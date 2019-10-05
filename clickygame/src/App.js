07260cd222bbc0856a154af6ff6f80b7208ac3a0
Elvia <evfranco@hush.ai>
2019-10-04 17:49:38 -0700

    Initial commit - static page

---
+++ clickygame/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import friends from "./friends.json";
+import NavBar from "./components/Navbar";
+import JumboTron from "./components/JumboTron";
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <div>
+      <NavBar />
+      <JumboTron />
+      <Wrapper>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+          />
+        ))}
+      </Wrapper>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ clickygame/src/components/Navbar/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./style.css";
+
+function NavBar() {
+    return (
+        <nav class="navbar navbar-light bg-light">
+            <ul class="nav">
+                <a class="navbar-brand" href="/">
+                    <img src="https://vignette.wikia.nocookie.net/friends/images/9/94/FriendsLogo.png/revision/latest/scale-to-width-down/256?cb=20180501094924" alt="logo"></img>
+                </a>
+            </ul>
+
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                   Click an image to begin!
+                </li>
+            </ul>
+
+            <ul class="nav justify-content-end">
+                <li class="nav-item">
+                    Score:  | Top Score:
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
