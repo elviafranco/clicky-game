ad70854ae6d8804c5c3b8f9076cd160092553a64
Elvia <evfranco@hush.ai>
2019-10-05 12:45:08 -0700

    Updates

---
+++ precache-manifest.05c142350dd572ee902d79c4e4de43a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9adb7cd4e38cd162375e1694cc3966",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "8bd300430327ef5be942",
+    "url": "/clicky-game/static/css/main.cae40291.chunk.css"
+  },
+  {
+    "revision": "06812dbacc4ac64fcb8b",
+    "url": "/clicky-game/static/js/2.70b968ce.chunk.js"
+  },
+  {
+    "revision": "8bd300430327ef5be942",
+    "url": "/clicky-game/static/js/main.bdce42b7.chunk.js"
+  },
+  {
+    "revision": "dcbdc37ead438a754843",
+    "url": "/clicky-game/static/js/runtime-main.5add7050.js"
+  },
+  {
+    "revision": "0214f1c8ea87b97882178c04358d8345",
+    "url": "/clicky-game/static/media/gabrwffr.0214f1c8.woff2"
+  },
+  {
+    "revision": "e0c9f2df54b8bd68cd15cc1ba5ab72fd",
+    "url": "/clicky-game/static/media/gabrwffr.e0c9f2df.woff"
+  }
+]);
